a42c6728f30c91996a8b765887b99ed1cab2584e
dhruva011 <dhruva011@gmail.com>
2020-07-02 13:20:16 +0530

    Adding More Paragraph

--- english/script.js
+++ english/script.js
@@ -1,8 +1,6 @@
 function setPara(){
-	var card = document.getElementById("difficuty").value;
+	var diff = document.getElementById("difficuty").value;
 	var x = document.getElementById("paragraph");
-	var option = document.createElement("option");
-	var option1 = document.createElement("option");
 	
 	//Remove all the element from paragraph drop down
 	var select = document.getElementById("paragraph");
@@ -10,28 +8,73 @@ function setPara(){
 	for (i = length-1; i >= 0; i--) {
 	  select.options[i] = null;
 	}
-		if (card == "Easy") {
-			option.text = "Easy Text1";
-			option1.text = "Easy Text2";
+	
+		if(diff == "NA"){
+			var option1 = document.createElement("option");
+			option1.text = "--Select--";
+			option1.value = "NA";
+			x.add(option1);
 		}
-		if (card == "Medium") {
-			option.text = "Medium Text1";
-			option1.text = "Medium Text2";
+		else if (diff == "Easy") {
+			var option1 = document.createElement("option");
+			var option2 = document.createElement("option");
+			option1.text = "Easy Text1";
+			option2.text = "Easy Text2";
+			x.add(option1);
+			x.add(option2);
 		}
-		if (card == "Hard") {
-			option.text = "Hard Text1";
-			option1.text = "Hard Text2";
+		else if (diff == "Medium") {
+			var option1 = document.createElement("option");
+			var option2 = document.createElement("option");
+			var option3 = document.createElement("option");
+			var option4 = document.createElement("option");
+			var option5 = document.createElement("option");
+
+			option1.text = "Medium Text1";
+			option2.text = "Medium Text2";
+			option3.text = "Medium Text3";
+			option4.text = "Medium Text4";
+			option5.text = "Medium Text5";
+			
+			x.add(option1);
+			x.add(option2);
+			x.add(option3);
+			x.add(option4);
+			x.add(option5);
 		}
-		if (card == "Editorial") {
-			option.text = "Editorial Text1";
-			option1.text = "Editorial Text2";
+		else if (diff == "Hard") {
+			var option1 = document.createElement("option");
+			var option2 = document.createElement("option");
+			var option3 = document.createElement("option");
+			var option4 = document.createElement("option");
+			var option5 = document.createElement("option");
+
+			option1.text = "Hard Text1";
+			option2.text = "Hard Text2";
+			option3.text = "Hard Text3";
+			option4.text = "Hard Text4";
+			option5.text = "Hard Text5";
+			
+			x.add(option1);
+			x.add(option2);
+			x.add(option3);
+			x.add(option4);
+			x.add(option5);
+		}
+		else if (diff == "Editorial") {
+			var option1 = document.createElement("option");
+			option1.text = "--Select--";
+			option1.value = "NA";
+			x.add(option1);
 		}
 	
-	x.add(option);
-	x.add(option1);
 }
 
 function proceed(){
+	//Validate if there are any improper selection
+	if(!validate())
+		return ;
+	
 	var e = document.getElementById("difficuty");
 	var dif = e.options[e.selectedIndex].text;
 	
@@ -48,8 +91,28 @@ function proceed(){
 	localStorage.setItem( 'isValid', true);
 	
 	location.href = "https://dhruva011.github.io/english/demo.html";
+	//location.href = "file:///C:/Users/Punit/Desktop/Profile/dhruva011.github.io/english/demo.html";
 }
 
 function reset(){
 	location.href = "https://dhruva011.github.io/english/";
+}
+
+function validate(){
+	var diff = document.getElementById("difficuty").value;
+	if(diff=="NA"){
+		alert("Please select appropriate difficulty");
+		return false;
+	}
+	var diff = document.getElementById("paragraph").value;
+	if(diff=="NA"){
+		alert("Please select appropriate paragraph");
+		return false;
+	}
+	var diff = document.getElementById("time").value;
+	if(diff=="NA"){
+		alert("Please select appropriate time");
+		return false;
+	}
+	return true;
 }
